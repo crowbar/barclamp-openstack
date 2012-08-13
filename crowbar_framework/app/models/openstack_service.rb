eebf4f4f0a0016f4db85790d7642614e9317665f
Greg Althaus <galthaus@austin.rr.com>
2012-08-12 21:57:55 -0500

    Convert routines over to new database methods

--- crowbar_framework/app/models/openstack_service.rb
+++ crowbar_framework/app/models/openstack_service.rb
@@ -1,4 +1,4 @@
-# Copyright 2011, Dell, Inc. 
+# Copyright 2012, Dell, Inc. 
 # 
 # Licensed under the Apache License, Version 2.0 (the "License"); 
 # you may not use this file except in compliance with the License. 
@@ -14,7 +14,5 @@
 # 
 
 class OpenstackService < ServiceObject
-
- 
 end
 
