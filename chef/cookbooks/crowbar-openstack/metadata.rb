d65d557bafbfbc451a897b71f71befe35327ff1c
Vincent Untz <vuntz@suse.com>
2014-08-11 17:08:43 +0200

    Add a crowbar-openstack cookbook
    The goal is to collect helpers that are useful accross all OpenStack
    cookbooks, but don't belong to the core of Crowbar.
    
    We start with helpers to fetch the information about the database server
    and the rabbitmq server.

---
+++ chef/cookbooks/crowbar-openstack/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Crowbar Project"
+maintainer_email "crowbar@dell.com"
+license          "Apache 2.0"
+description      "Help install/configure OpenStack, deployed by Crowbar"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1"
+
+depends "database"
